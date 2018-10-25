5cfffca8e6b506b2ab2e4405556fbc52f12b19ad
julius <julius.bischoff@martaflex.de>
2018-10-25 21:32:37 +0200

    moved helper functions

--- src/main/routes/form-fill.kt
+++ src/main/routes/form-fill.kt
@@ -1,5 +1,7 @@
 package de.martaflex.nanopdf.routes
 
+import de.martaflex.nanopdf.helpers.*
+
 import java.io.*
 import java.util.Base64
 
@@ -15,44 +17,6 @@ import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 
-val toJson = fun (o: Any) : String {
-    val mapper = ObjectMapper();
-    return mapper.writeValueAsString(o);
-}
-
-// FIXME: json response on halt
-data class Error(
-    val status: Int,
-    val message: String,
-    val reason: String
-);
-
-// FIXME: move helper functions somewhere else
-fun fromJson (body : String) : JsonNode? {
-    try {
-        val mapper = ObjectMapper();
-        return mapper.readTree(body);
-    }
-    catch (e : JsonParseException) {
-        halt(400, e.message)
-    }
-    return null;
-}
-
-fun fromBase64 (s : String) : ByteArray? {
-    try {
-        return Base64.getDecoder().decode(s);
-    }
-    catch (e : IllegalArgumentException) {
-        halt(400, e.message)
-    }
-    return null;
-}
-
-fun toBase64 (bytes : ByteArray) : String {
-    return String(Base64.getEncoder().encode(bytes));
-}
-
 fun FormFill () {
     post("/form-fill", "application/json", fun(request, response) : Any {
         val body = request.body();
@@ -63,7 +27,7 @@ fun FormFill () {
 
         // FIXME: im basically 100%sure that this will never be null
         val json = fromJson(body)!!;
-        
+
         // FIXME: move this shit somewhere else
         if (json.get("pdf") == null) {
             halt(400, "pdf parameter required");
@@ -84,7 +48,7 @@ fun FormFill () {
             val reader = PdfReader(pdf);
             val buffer = ByteArrayOutputStream();
             val stamper = PdfStamper(reader, buffer);
-            
+
             val form = stamper.getAcroFields();
             form.setGenerateAppearances(true); // if not set the fields will be empty when flattening
             val values = json.get("data");
@@ -105,7 +69,7 @@ fun FormFill () {
             println(e.message);
             halt(400, e.message)
         }
-        
+
         halt(500, "this is not spposed to happen");
         // FIXME: wait what??
         return ""
