9bb904cb3c53fda7664412b3f12a22f84f51ef5b
throo <throo@protonmail.com>
2023-12-15 11:02:17 +0100

    added get-page-dimensions test

---
+++ src/test/routes/get-page-dimensions.kt
@@ -0,0 +1,108 @@
+package de.martaflex.nanopdf.routes
+
+import java.io.*
+import java.util.Base64
+import java.util.concurrent.TimeUnit
+import java.util.Arrays
+
+import org.junit.*
+import org.junit.Assert.*
+
+import spark.Spark.*
+import com.mashape.unirest.http.*
+
+class GetPageDimensionsTest {
+    companion object {
+        @BeforeClass @JvmStatic
+        fun setup () {
+            port(9091)
+            GetPageDimensions();
+            // wait for spark to be initialized
+            awaitInitialization()
+        }
+
+        @AfterClass @JvmStatic
+        fun teardown () {
+            stop();
+            // give spark some time to shut down
+            // FIXME: there is not method for waiting til spark server is stopped
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    @Test
+    fun noPostBody () {
+        val response = Unirest.post("http://127.0.0.1:9091/get-page-dimensions")
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .asString()
+
+        assertEquals(400, response.getStatus());
+        println(response.getBody());
+    }
+
+    @Test
+    fun emptyPostBody () {
+        val response = Unirest.post("http://127.0.0.1:9091/get-page-dimensions")
+            .header("Content-Type", "application/json")
+            .body("")
+            .asString()
+
+        assertEquals(400, response.getStatus());
+        println(response.getBody());
+    }
+
+    @Test
+    fun invalidPostBody () {
+        val response = Unirest.post("http://127.0.0.1:9091/get-page-dimensions")
+            .header("Content-Type", "application/json")
+            .body("""{ "k1": "v1" }""")
+            .asString()
+
+        assertEquals(400, response.getStatus());
+        println(response.getBody());
+    }
+
+    @Test
+    fun invalidBase64 () {
+        val response = Unirest.post("http://127.0.0.1:9091/get-page-dimensions")
+            .header("Content-Type", "application/json")
+            .body("""
+            {
+                "pdf": "asdasdadasd",
+                "data": {
+                    "k1": "v1"
+                }
+            }
+            """)
+            .asString()
+
+        assertEquals(400, response.getStatus());
+        println(response.getBody());
+    }
+
+    @Test
+    fun validPost () {
+        val pdf = File("resources/test/to-text.pdf").readBytes();
+
+        val pdf64 = String(Base64.getEncoder().encode(pdf));
+
+        val response = Unirest.post("http://127.0.0.1:9091/get-page-dimensions")
+            .header("Content-Type", "application/json")
+            .body("""
+            {
+                "pdf": "${ pdf64 }"
+            }
+            """)
+            .asString()
+
+        assertEquals(200, response.getStatus())
+
+        val retrieved = response.getBody()
+
+        val expected = "[[0.0,0.0,595.0,842.0],[0.0,0.0,595.0,842.0]]"
+
+        assertEquals(expected, retrieved)
+    }
+
+}
