b3e9dedf5cbf7b435eae6ff282936ec986c18f41
throo <throo@protonmail.com>
2024-02-13 21:13:23 +0100

    changed type results of matches; set ignore_case flag

--- src/main/routes/regex-to-text.kt
+++ src/main/routes/regex-to-text.kt
@@ -3,56 +3,50 @@ package de.martaflex.nanopdf.routes
 import de.martaflex.nanopdf.helpers.*
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.text.PDFTextStripper
-import org.apache.pdfbox.text.PDFTextStripperByArea
 import spark.Spark.*
-import java.awt.Rectangle
 import java.util.ArrayList
-import java.util.regex.MatchResult
-import java.util.regex.Pattern
-
-data class RegexData(val key: String, val regex: Pattern)
 
 fun RegexToText() {
     post("/regex-to-text", "application/json", fun(request, response) : Any {
-        val body = request.body();
+        val body = request.body()
 
         if (body == "") {
-            halt(400, "Empty Request Body");
+            halt(400, "Empty Request Body")
         }
 
         // FIXME: im basically 100%sure that this will never be null
-        val json = fromJson(body)!!;
+        val json = fromJson(body)!!
 
         if (json.get("pdf") == null) {
-            halt(400, "pdf parameter required");
+            halt(400, "pdf parameter required")
         }
 
         if (json.get("data") == null) {
-            halt(400, "data parameter required");
+            halt(400, "data parameter required")
         }
 
         if (!(json.get("data").isObject())) {
-            halt(400, "data parameter must be a list");
+            halt(400, "data parameter must be a list")
         }
 
         // FIXME: im basically 100%sure that this will never be null
         val pdf = fromBase64(json.get("pdf").asText())!!
-        val values = json.get("data");
+        val values = json.get("data")
 
         try {
-            val doc = Loader.loadPDF(pdf);
-            val stripper = PDFTextStripper();
-            stripper.sortByPosition = true;
+            val doc = Loader.loadPDF(pdf)
+            val stripper = PDFTextStripper()
+            stripper.sortByPosition = true
 
             //regex patterns
             val regexPatterns: MutableMap<String, Regex> = mutableMapOf<String, Regex>()
 
             for (key in values.fieldNames()) {
-                val regexString = values.get(key).asText()
-                regexPatterns[key] = Regex(regexString)
+                val patternString = values.get(key).asText()
+                regexPatterns[key] = Regex(patternString, RegexOption.IGNORE_CASE)
             }
 
-            val results = ArrayList<MutableMap<String, List<kotlin.text.MatchResult>>>()
+            val results = ArrayList<MutableMap<String, ArrayList<String>>>()
 
 
             // Process each page and extract matches for each regex pattern
@@ -60,13 +54,17 @@ fun RegexToText() {
                 stripper.startPage = i
                 stripper.endPage = i
                 val text = stripper.getText(doc)
-                val matches: MutableMap<String, List<kotlin.text.MatchResult>> = mutableMapOf<String, List<kotlin.text.MatchResult>>()
+                val matches: MutableMap<String, ArrayList<String>> = mutableMapOf<String, ArrayList<String>>()
 
                 for (regex in regexPatterns.iterator()) {
-                    matches[regex.key] = regex.value.findAll(text).toList()
+                    val test = ArrayList<String>()
+                    for ( match in regex.value.findAll(text).iterator()) {
+                        test.add(match.value)
+                    }
+
+                    matches[regex.key] = test
                 }
                 results.add(matches)
-
             }
 
             response.type("application/json")
@@ -80,5 +78,5 @@ fun RegexToText() {
         halt(500, "this is not supposed to happen")
         // FIXME: wait what??
         return ""
-    });
+    })
 }
